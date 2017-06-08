1c04386ca0abc7521ac060b89ab7c66c33190429
KoheiBun <localhost.ko@gmail.com>
2017-06-08 19:18:03 +0900

    fix database.class

--- class/DB.class.php
+++ class/DB.class.php
@@ -4,7 +4,7 @@
 
 class DB {
 
-    const DB_HOST = 'postgres:dbname=d7uunh1sb58rvn;host=c2-54-225-68-71.compute-1.amazonaws.com;charset=utf8';
+    const DB_HOST = 'pgsql:dbname=d7uunh1sb58rvn; host=c2-54-225-68-71.compute-1.amazonaws.com; charset=utf8';
     const DB_USER = 'hmpzvzhlyhlaeb';
     const DB_PASSWORD = 'c6e1b4152cce054e06b5a52dc2b38169cb5fdae834887a40c1751645bb3ba981';
     const DB_OPTIONS = array(

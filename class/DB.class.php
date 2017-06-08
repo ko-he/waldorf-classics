0b2d0ac85e809801168a22b39b972af2f71f3299
KoheiBun <localhost.ko@gmail.com>
2017-06-08 19:42:19 +0900

    create database.class

--- class/DB.class.php
+++ class/DB.class.php
@@ -3,10 +3,12 @@
 2017.04.07*/
 
 class DB {
+    $url = parse_url(getenv('DATABASE_URL'));
 
-    const DB_HOST = 'pgsql:dbname=d7uunh1sb58rvn host=ec2-54-225-68-71.compute-1.amazonaws.com port=5432 charset=utf8';
-    const DB_USER = 'hmpzvzhlyhlaeb';
-    const DB_PASSWORD = 'c6e1b4152cce054e06b5a52dc2b38169cb5fdae834887a40c1751645bb3ba981';
+    var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+    const DB_HOST = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+    const DB_USER = $url['user'];
+    const DB_PASSWORD = $url['pass'];
     const DB_OPTIONS = array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,

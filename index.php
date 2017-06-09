b7a2528ae3b4339638b7dabb17b486f10b137af5
KoheiBun <localhost.ko@gmail.com>
2017-06-09 19:14:28 +0900

    create indexpage

--- index.php
+++ index.php
@@ -3,4 +3,18 @@ require 'class/DB.class.php';
 
 $db = new DB();
 
+$sql = 'SELECT * FROM schedules WHERE sc_date>=NOW() ORDER BY sc_date ASC LIMIT 10';
+$recode = $db->queryPost($sql, array());
+$row = $db->dbFetch($recode);
+print_r($row)
  ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+    </body>
+</html>

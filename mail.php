541f420c871cd58e9437f58fb367bea45e0c0726
KoheiBun <localhost.ko@gmail.com>
2017-06-09 10:04:04 +0900

    create schedul class, require mailgrid

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+// If you are using Composer
+require 'vendor/autoload.php';
+
+// If you are not using Composer (recommended)
+// require("path/to/sendgrid-php/sendgrid-php.php");
+
+$from = new SendGrid\Email(null, "localhost.ko@gmail.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "joujisaru@yahoo.co.jp");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('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');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();

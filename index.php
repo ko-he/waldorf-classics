85221e1202b4386dba53f84a8d398d954b88230d
KoheiBun <localhost.ko@gmail.com>
2017-06-10 22:29:19 +0900

    create sc list

--- index.php
+++ index.php
@@ -16,14 +16,22 @@ $schedules = $schedule->getSchedules();
                 <div class="top">
                     <div class="next-match">
                         <p class="label">次の試合</p>
-                        <p class="match-name"><?php echo h($next_match['description']); ?></p>
-                        <p class="place"><?php echo h($next_match['place']); ?></p>
-                        <p class="date-time"><span class="date"><?php echo h(substr($next_match['sc_date'], 5)); ?></span><span class="time"><?php echo h(substr($next_match['start_time'], 0, 5)); ?></span>~</p>
+                        <?php if(!empty($next_match)): ?>
+                            <p class="match-name"><?php echo h($next_match['description']); ?></p>
+                            <p class="place"><?php echo h($next_match['place']); ?></p>
+                            <p class="date-time"><span class="date"><?php echo h(substr($next_match['sc_date'], 5)); ?></span><span class="time"><?php echo h(substr($next_match['start_time'], 0, 5)); ?></span>~</p>
+                        <?php else: ?>
+                            <p class="no-sc">未定</p>
+                        <?php endif; ?>
                     </div>
                     <div class="next-plactis">
                         <p class="label">次の練習</p>
-                        <p class="place"><?php echo h($next_practice['place']); ?></p>
-                        <p class="date-time"><span class="date"><?php echo h(dateformat($next_practice['sc_date'])); ?></span><span class="time"><?php echo h(substr($next_practice['start_time'], 0, 5)); ?></span>~<span class="time"><?php echo h(substr($next_practice['finish_time'], 0, 5)); ?></span></p>
+                        <?php if(!empty($next_practice)): ?>
+                            <p class="place"><?php echo h($next_practice['place']); ?></p>
+                            <p class="date-time"><span class="date"><?php echo h(dateformat($next_practice['sc_date'])); ?></span><span class="time"><?php echo h(substr($next_practice['start_time'], 0, 5)); ?></span>~<span class="time"><?php echo h(substr($next_practice['finish_time'], 0, 5)); ?></span></p>
+                        <?php else: ?>
+                            <p class="no-sc">未定</p>
+                        <?php endif; ?>
                     </div>
                 </div>
                 <h2 id="schedule">Schedule</h2>
@@ -31,14 +39,14 @@ $schedules = $schedule->getSchedules();
                     <?php foreach($schedules as $value): ?>
                         <?php if($value['sc_type'] == 1): ?>
                             <li class="schedule practise">
-                                <p class="date"><?php echo h($value['sc_date']); ?></p>
+                                <p class="date"><?php echo h(dateformat($value['sc_date'])); ?></p>
                                 <p class="time">時間：<span><?php echo h($value['start_time']); ?></span>~<span ><?php echo h($value['finish_time']); ?></span></p>
                                 <p class="place">場所：<?php echo h($value['place']); ?></p>
                                 <p class="label">練習</p>
                             </li>
                         <?php else: ?>
                             <li class="schedule match">
-                                <p class="date"><?php echo h($value['sc_date']); ?></p>
+                                <p class="date"><?php echo h(dateformat($value['sc_date'])); ?></p>
                                 <p class="time">時間：<span><?php echo h($value['start_time']); ?></span>~</p>
                                 <p class="place">場所：<?php echo h($value['place']); ?></p>
                                 <p class="label">試合</p>

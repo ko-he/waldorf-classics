b8397f81f85dc501deaad6b07571f7169b08e675
KoheiBun <localhost.ko@gmail.com>
2017-06-11 09:29:54 +0900

    mypae updated

--- mypage.php
+++ mypage.php
@@ -11,5 +11,8 @@ $user = new User();
 $prof = $user->getUser($_SESSION['id']);
  ?>
 <?php require '_include/header.php'; ?>
-        
+    <h1>Waldorf Classics</h1>
+    <p class="menu"><img src="images/menu.png" alt=""></p>
+</header>
+
  <?php require '_include/footer.php'; ?>

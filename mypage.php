908052d4fa26471ceb1d5aeb8881ce5968887cf2
KoheiBun <localhost.ko@gmail.com>
2017-06-11 09:36:12 +0900

    css updated

--- mypage.php
+++ mypage.php
@@ -22,24 +22,14 @@ $prof = $user->getUser($_SESSION['id']);
             <p class="email">E-mail：<?php echo h($prof['email']); ?></p>
             <p class="btn"><a href="">編集</a></p>
         </div>
-        <div class="content">
-            <div class="prof-box">
-                <div class="prof">
-                    <p class="title">登録情報</p>
-                    <p class="name">Name：<?php echo h($prof['name']); ?></p>
-                    <p class="email">E-mail：<?php echo h($prof['email']); ?></p>
-                    <p class="btn"><a href="">編集</a></p>
-                </div>
-                <div class="line">
-                    <p class="title">LINE を利用する</p>
-                    <?php if(empty($user['lien_id'])): ?>
-                        <p class="line-code">承認コード：<?php echo h($prof['line_code']); ?></p>
-                        <p class="line-btn"><a href="https://line.me/R/ti/p/%40qyf6351f"><img height="40" border="0" alt="友だち追加" src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"></a></p>
-                    <?php else: ?>
-                        <p>承認済み</p>
-                    <?php endif; ?>
-                </div>
-            </div>
+        <div class="line">
+            <p class="title">LINE を利用する</p>
+            <?php if(empty($user['lien_id'])): ?>
+                <p class="line-code">承認コード：<?php echo h($prof['line_code']); ?></p>
+                
+            <?php else: ?>
+                <p>承認済み</p>
+            <?php endif; ?>
         </div>
     </div>
 </div>

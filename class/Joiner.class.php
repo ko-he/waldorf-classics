806da507c578b4ed5b2f57f63d297235db32312a
KoheiBun <localhost.ko@gmail.com>
2017-06-14 11:55:37 +0900

    modified Line Prof

--- class/Joiner.class.php
+++ class/Joiner.class.php
@@ -13,10 +13,6 @@ class Joiner
     {
         $this->db = new DB();
 
-        // line api インスタンス生成
-        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('w9AHFGgDfuiXizJ+nbUgospTqb7uTy5hr+viE+KAo66P5SZf2wP7x0yEtUceun+7RGhZ7HyAmF0yS+kMA8P5EQ3DZKweK/wuMPuALf7PWo85JjVAgZ9IrMKDtirjfeVe6Yxyz/FAXgMN/sNK1HqDeQdB04t89/1O/w1cDnyilFU=');
-        $this->bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '
-        35cfb80864e3be2e8c3377689e7dd3a7']);
 
     }
 
@@ -24,7 +20,11 @@ class Joiner
     {
         //line user id から profile を取得
         if(!empty($line_id)){
-            $response = $this->bot->getProfile($line_id);
+            // line api インスタンス生成
+            $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('w9AHFGgDfuiXizJ+nbUgospTqb7uTy5hr+viE+KAo66P5SZf2wP7x0yEtUceun+7RGhZ7HyAmF0yS+kMA8P5EQ3DZKweK/wuMPuALf7PWo85JjVAgZ9IrMKDtirjfeVe6Yxyz/FAXgMN/sNK1HqDeQdB04t89/1O/w1cDnyilFU=');
+            $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '
+            35cfb80864e3be2e8c3377689e7dd3a7']);
+            $response = $->getProfile($line_id);
             if ($response->isSucceeded()) {
                 $profile = $response->getJSONDecodedBody();
                 $img_url = $profile['pictureUrl'];

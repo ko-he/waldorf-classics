36e2a91e2c430b6279dfaf1db6fe9dcbe590ebb9
KoheiBun <localhost.ko@gmail.com>
2017-06-22 23:25:18 +0900

    teimformat function updated.

--- function.php
+++ function.php
@@ -12,7 +12,7 @@ function csrfSetToken(){
 }
 
 function timeFormat($time){
-    if(!empty($time)){
+    if($time != '00:00:00'){
         return substr($time, 0, 5);
     }else{
         return '未定';
